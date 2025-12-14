7542dbea1e90417d8e00e15b9512bdeb2987892d
OlesiaKovalchuk <olesiakovalchuk03@gmail.com>
2025-12-14 20:54:57 +0200

    Initial commit

---
+++ page-objects/formLayoutsPage.ts
@@ -0,0 +1,46 @@
+
+import { Page } from "@playwright/test";
+import { HelperBase } from "./helperBase";
+
+
+export class FormLayoutsPage extends HelperBase{
+
+      constructor(page:Page){
+        super(page)
+    }
+
+    async submitUsingTheGridFormWithCredentialsAndSelectOption(email:string, password:string, optionText:string){
+        const usingTheGridForm = this.page.locator('nb-card', {hasText:"Using the Grid"})
+        await usingTheGridForm.getByRole("textbox",{name:"Email"}).fill(email)
+        await usingTheGridForm.getByRole("textbox",{name:"Password"}).fill(password)
+        await usingTheGridForm.getByRole('radio', {name:optionText}).check({force:true})
+        await usingTheGridForm.getByRole("button").click()
+
+    }
+
+/**
+ * This method fills out inline form with user details
+ * @param name  - should be name and surname 
+ * @param email - valid email for the test user
+ * @param rememberMe - true or false if user session to be safed
+ */
+
+    async submitInlineFormWithNameEmailAndCheckbox(name: string, email:string, rememberMe:boolean){
+      const inlineForm = this.page.locator('nb-card', {hasText:"Inline form"})
+      await inlineForm.getByRole('textbox',{name:"Jane Doe"}).fill(name)
+      await inlineForm.getByRole("textbox",{name:"Email"}).fill(email)
+      if(rememberMe)
+      await inlineForm.getByRole("checkbox",{name:"Remember me"}).check({force:true})
+      await inlineForm.getByRole("button").click()
+    }
+
+    async submitBasicFormWithEmailPasswordAndCheckbox(email:string, password:string, checkMeOut: boolean){
+        const basicForm = this.page.locator('nb-card', {hasText:"Basic form"})
+        await basicForm.getByRole('textbox',{name:"Email address"}).fill(email)
+        await basicForm.getByRole('textbox',{name:"Password"}).fill(email)
+        if(checkMeOut)
+        await basicForm.getByRole('checkbox', {name:"Check me out"}).check({force:true})
+        await basicForm.getByRole('button',{name:"Submit"}).click()
+        
+}}
+
---
+++ playwright.config.ts
@@ -0,0 +1,85 @@
+import { defineConfig, devices } from '@playwright/test';
+import type {TestOptions} from './test-options'
+
+
+require ('dotenv').config();
+
+export default defineConfig<TestOptions>({
+  timeout: 40000,
+  //globalTimeout: 60000,
+  expect:{
+   timeout: 2000
+  },
+
+  retries: 1,
+   reporter: [
+    ['json',{outputFile:'test-results/jsonReporter.json'}],
+    ['junit',{outputFile:'test-results/junitReporter.xml'}],
+   // ['allure-playwright'],
+    ['html']
+],
+   
+ 
+  use: {
+
+  globalsQaURL:'https://www.globalsqa.com/demo-site/draganddrop/',
+  baseURL:process.env.DEV === '1' ? 'http://localhost:4200/'
+         : process.env.STAGING ==='1' ? 'http://localhost:4202/'
+         : 'http://localhost:4200/',
+
+   
+    trace: 'on-first-retry',
+    //actionTimeout: 5000,
+    navigationTimeout: 5000,
+    video: {
+      mode:"off",
+      size:{width: 1920, height:1080}
+  }
+  },
+
+  projects: [
+     {
+      name: 'dev',
+      use: { 
+        ...devices['Desktop Chrome'],
+      baseURL: 'http://localhost:4201/',
+      },
+    },
+     
+    {
+      name: 'chromium',     
+    },
+
+    {
+      name: 'firefox',
+      use: { 
+        browserName: 'firefox',
+        video: {
+      mode:"off",
+      size:{width: 1920, height:1080}
+  }
+       },
+    },
+    {
+      name:'pageObjectFullScreen',
+      testMatch: 'usePageObjects.spec.ts',
+      use:{
+        viewport:{width: 1920, height:1080 }
+      }
+    }, 
+    {
+      name: 'mobile',
+      testMatch:'testMobile.spec.ts',
+      use:{
+        ...devices['iPhone 15 Pro Max']
+      }
+    }
+    
+  ],
+
+  webServer:{
+    command:'npm run start',
+    url:'http://localhost:4200/'
+  }
+
+});
